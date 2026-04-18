ab3f4eaa1273823b707191a767a9f3c8038055a8
DhruvGautam099 <dhruvgautam98765@gmail.com>
2026-04-18 22:57:40 +0530

    First lc88 commit

---
+++ lc88.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int []arr=new int[m+n];
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<m&&j<n){
+            if(nums1[i]<=nums2[j]){
+                arr[k]=nums1[i];
+                i++;
+            }
+            else{
+                arr[k]=nums2[j];
+                j++;
+            }
+            k++;
+        }
+        {
+            while(i<m){
+                arr[k]=nums1[i];
+                k++;
+                i++;
+            }
+        }{
+            while(j<n){
+                arr[k]=nums2[j];
+                j++;
+                k++;
+            }
+        }
+        i=0;
+        for(int item:arr){
+            nums1[i]=item;
+            i++;
+        }
+    }
+}
